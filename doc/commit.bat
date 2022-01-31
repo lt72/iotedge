4e57e26f922b428a5a54ac4169392ea971984ffa
lt72 <lotessio@microsoft.com>
2022-01-31 13:55:35 -0800

    xxx

--- doc/commit.bat
+++ doc/commit.bat
@@ -1,4 +1,4 @@
 git pull origin master 
-git add md/images/AzureIoTEdgeRuntime__architecture__depl_diag.png md/resources/AzureIoTEdgeRuntime__architecture__depl_diag.uml
+git add commit.bat md/resources/AzureIoTEdgeRuntime__architecture__comp_diag.uml md/images/AzureIoTEdgeRuntime__architecture__comp_diag.png md/images/AzureIoTEdgeRuntime__architecture__depl_diag.png md/resources/AzureIoTEdgeRuntime__architecture__depl_diag.uml
 git commit -m "xxx"
 git push origin master
